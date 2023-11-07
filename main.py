32f9eaf00bb38ec72f343bfc8495e4ec8d5484cd
TheEngineerOne <verbruggenlucas2@gmail.com>
2023-11-07 21:23:31 +0100

    Changing dates formats from YYYYMMDD HH:mm:ss to YYYYMMDDTHH:mm:ss

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ outFile.write("<array name=\"event_list\">\n")
 indentationLevel += 1
 
 def convertDate(date):
-    return f"{date[0:4]}-{date[4:6]}-{date[6:8]} {date[9:11]}:{date[11:13]}:{date[13:15]}"
+    return f"{date[0:4]}-{date[4:6]}-{date[6:8]}T{date[9:11]}:{date[11:13]}:{date[13:15]}"
     
 
 for line in inFile:
